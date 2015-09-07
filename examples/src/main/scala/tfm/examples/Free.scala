9c8278e4a67c07e66c56780f8b31ab7963d382dc
Adelbert Chang <adelbertc@gmail.com>
2015-09-07 14:43:39 -0700

    FreeMonoid example

--- examples/src/main/scala/tfm/examples/Free.scala
+++ examples/src/main/scala/tfm/examples/Free.scala
@@ -1,6 +1,6 @@
 package tfm.examples
 
-import cats.{Applicative, Id}
+import cats.{Applicative, Id, Monoid}
 import cats.implicits._
 
 import tfm.fin
@@ -37,4 +37,36 @@ object ApplicativeApp extends App {
     """
 
   println(output)
+}
+
+@fin("FreeMonoid")
+trait MonoidInterpreter[F[_]] {
+  def nil[A]: F[A]
+  def cons[A](head: A, tail: F[A]): F[A]
+}
+
+object MonoidInterpreter {
+  type FoldMap[A] = { type F[X] = (X => A) => A }
+
+  def foldMap[B](implicit B: Monoid[B]): MonoidInterpreter[FoldMap[B]#F] =
+    new MonoidInterpreter[FoldMap[B]#F] {
+      def nil[A]: (A => B) => B = _ => B.empty
+      def cons[A](lhs: A, rhs: (A => B) => B): (A => B) => B =
+        (map: A => B) => B.combine(map(lhs), rhs(map))
+    }
+}
+
+object MonoidApp extends App {
+  import MonoidInterpreter._
+  import MonoidInterpreter.language._
+
+  val program1 = nil[Int]
+  val result1 = program1.run[FoldMap[Int]#F](foldMap[Int])(identity) // 0
+
+  val program2 = cons(1, cons(2, cons(3, nil)))
+  val result2 = program2.run[FoldMap[Int]#F](foldMap[Int])(identity) // 6
+
+  val result3 = program2.run[FoldMap[List[Int]]#F](foldMap[List[Int]])(List(_)) // List(1, 2, 3)
+
+  println(s"result1 = ${result1}\nresult2 = ${result2}\nresult3 = ${result3}")
 }
